475de19ea049145069b3dd35d86263470c18fe04
Dinesh Davala <davaladinesh34@gmail.com>
2022-07-29 09:19:54 +0530

    power_ABC.py 'power_program_scaler

---
+++ power_ABC.py
@@ -0,0 +1,23 @@
+def power(x, y, p) :
+    res = 1     # Initialize result
+ 
+    # Update x if it is more
+    # than or equal to p
+    x = x % p
+     
+    if (x == 0) :
+        print(0)
+ 
+    while (y > 0) :
+         
+        # If y is odd, multiply
+        # x with result
+        if ((y & 1) == 1) :
+            res = (res * x) % p
+ 
+        # y must be even now
+        y = y >> 1      # y = y/2
+        x = (x * x) % p
+         
+    print(res)
+power(-1,1,20)
