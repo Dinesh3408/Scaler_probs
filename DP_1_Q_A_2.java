efd8d9a565d5b7d963f4edae7ba910c2e544d4a5
Dinesh3408 <davaladinesh34@gmail.com>
2022-12-06 09:04:09 +0530

    Fibonacci Number..

--- DP_1_Q_A_2.java
+++ DP_1_Q_A_2.java
@@ -8,3 +8,30 @@ public class DP_1_Q_A_2 {
     }
     
 }
+
+//The above code gives the time complexity of O(2^2) as we are calculating the same funtion again and again. This is called overlapping sub problems. This can be avoid by using storing already calcualted results in a storage.
+// This process is called memoization.
+//Since, in fibonacii, the current number is explicitly depending upon previous two numbers , we are using two variables to to solve the problem.
+
+//The above code cabe rewritten as
+
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    # I am choosing bottom up approach to solve the problem
+    n = int(input())
+    first  = 0
+    second = 1
+    ans = 1
+    for i in range(2, n+1):
+        ans = first + second
+        first = second
+        second = ans
+    print(ans)
+
+
+    return 0
+
+if __name__ == '__main__':
+    main()
