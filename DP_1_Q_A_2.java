203d77203e6e7abbd73ed9feccc12b4f2bc8c064
Dinesh3408 <davaladinesh34@gmail.com>
2022-12-06 08:53:10 +0530

    Fibonacci Number

---
+++ DP_1_Q_A_2.java
@@ -0,0 +1,10 @@
+public class DP_1_Q_A_2 {
+    public static long fib(long n){
+        if(n == 0 || n == 1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args){
+        System.out.println(fib(10));
+    }
+    
+}
