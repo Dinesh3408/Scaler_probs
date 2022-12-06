f5205647bb2324e317468ea38e2acc61413664eb
Dinesh3408 <davaladinesh34@gmail.com>
2022-12-06 10:02:18 +0530

    Fibonacci Number...

--- DP_1_Q_A_2.java
+++ DP_1_Q_A_2.java
@@ -9,11 +9,20 @@ public class DP_1_Q_A_2 {
     
 }
 
-//The above code gives the time complexity of O(2^2) as we are calculating the same funtion again and again. This is called overlapping sub problems. This can be avoid by using storing already calcualted results in a storage.
+//The above code gives the time complexity of O(2^n) where n is the the input number.Since, we are calculating the same function repeatedly which in turn leading to take more time to get the result. This is called overlapping sub problem. This can be avoid by using storing already calcualted results in a storage.
 // This process is called memoization.
+
+//I know the above does not make sense at first glance, but for more details, please go through the below link.
+
+//https://www.geeksforgeeks.org/overlapping-subproblems-property-in-dynamic-programming-dp-1/
+
 //Since, in fibonacii, the current number is explicitly depending upon previous two numbers , we are using two variables to to solve the problem.
 
-//The above code cabe rewritten as
+// To tackle, overlapping subproblems, there are two approaches
+// 1. Topdown (Memoization) - We use recursion and storing the values in array(or lookup table) which are already been calculated for later use while writing code.
+// 2. BottomUp (Tabulation) - We are totallly depending upon the array and storing the values from bottom to top and using them when required for processing.
+
+//The above code cabe rewritten as following.
 
 def main():
     # YOUR CODE GOES HERE
@@ -35,3 +44,29 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+// Topdown Approach using Java
+
+public class fibonacci_topdown {
+    int n;
+    int[] lookup;
+    fibonacci_topdown(int n){ // used constructor initialize lookup array
+        this.n = n;
+        this.lookup = new int[n+1];
+    }
+
+  
+    int fib(int n){
+        if(n <= 1) lookup[n] = n;
+        else lookup[n] = fib(n-1) + fib(n-2);
+        return lookup[n];
+    }
+    public static void main(String[] args){
+        int n = 40;
+        //int[] lookup = new int[n+1];
+        fibonacci_topdown f = new fibonacci_topdown(n);
+        System.out.println(f.fib(n));
+    }
+
+
